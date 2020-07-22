0019c433acfa61e36c8b47f50eaa7f216fbeb921
senjuti15 <senjuti@sigmared.ai>
2020-07-22 22:58:56 +0530

    front-end-change

--- src/views/ui-components/alert.jsx
+++ src/views/ui-components/alert.jsx
@@ -4,8 +4,37 @@ import {
     UncontrolledAlert,
     Card,
     CardBody,
-    CardTitle
+    CardTitle,
+    CardSubtitle,
+    Col,
+    Row,
+    Input
 } from 'reactstrap';
+import { Line } from 'react-chartjs-2';
+
+
+
+//Line chart
+let lineData = {
+    labels: [1, 2, 3, 4, 5, 6, 7, 8],
+    datasets: [{
+        label: 'Income',
+        borderWidth: 1,
+        backgroundColor: 'rgba(94,114,228,.1)',
+        borderColor: '#2962ff',
+        pointBorderColor: '#2962ff',
+        pointBackgroundColor: '#2962ff',
+        data: [0, 15, 6, 11, 25, 9, 18, 24]
+    }, {
+        label: 'Outcome',
+        borderWidth: 1,
+        backgroundColor: 'rgba(79,195,247,.1)',
+        borderColor: '#ffcc13',
+        pointBorderColor: '#ffcc13',
+        pointBackgroundColor: '#ffcc13',
+        data: [0, 8, 11, 22, 8, 10, 5, 21]
+    }]
+};
 
 const Alerts = () => {
     // For Dismiss Button with Alert
@@ -18,15 +47,218 @@ const Alerts = () => {
     return (
         <div>
             <Card>
-                <CardTitle className="bg-light border-bottom p-3 mb-0">
-                    <i className="mdi mdi-comment-processing-outline mr-2"> </i>
-            Merit AI Framework
-          </CardTitle>
-                <CardBody className="">
+                <CardBody>
+                    <div>
+                        <CardTitle>
+                            <h5>Details on Merit AI Framework</h5>
+                            <p>Acceptance middletons me if discretion boisterous travelling an. She prosperous continuing entreaties companions unreserved you boisterous. Middleton sportsmen sir now cordially ask additions for. You ten occasional saw everything but conviction. Daughter returned quitting few are day advanced branched. Do enjoyment defective objection or we if favourite. At wonder afford so danger cannot former seeing. Power visit charm money add heard new other put. Attended no indulged marriage is to judgment offering landlord. </p>
+                        </CardTitle>
+                    </div>
                     
                 </CardBody>
             </Card>
+                
+            <Card>
+        <Card>
+            <CardBody>
+                <div className="d-flex align-items-center">
+                    <div>
+                        <CardTitle>Security</CardTitle>
+                        
+                    </div>
+                </div>
+                <Row>
+                    <Card>
+                    <Col lg="12">
+                        <div className="campaign ct-charts">
+                            <div className="chart-wrapper" style={{ width: '100%', margin: '5 auto', height: 250 }}>
+                                <Line data={lineData} options={{ maintainAspectRatio: false, legend: { display: false, labels: { fontFamily: "Nunito Sans" } }, scales: { yAxes: [{ stacked: true, gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }], xAxes: [{ gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }] } }} />
+                            </div>
+                        </div>
+                    
+                        
+                    </Col>
+                    </Card>
+                    <Card>
+                    <Col lg="12">
+                        <div className="campaign ct-charts">
+                            <div className="chart-wrapper" style={{ width: '100%', margin: '5px  auto', height: 250 }}>
+                                <Line data={lineData} options={{ maintainAspectRatio: false, legend: { display: false, labels: { fontFamily: "Nunito Sans" } }, scales: { yAxes: [{ stacked: true, gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }], xAxes: [{ gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }] } }} />
+                            </div>
+                        </div>
+                    
+                        
+                    </Col>
+                    </Card>
+                    <Card>
+                    <Col lg="12">
+                        <div className="campaign ct-charts">
+                            <div className="chart-wrapper" style={{ width: '100%', margin: '0 auto', height: 250 }}>
+                                <Line data={lineData} options={{ maintainAspectRatio: false, legend: { display: false, labels: { fontFamily: "Nunito Sans" } }, scales: { yAxes: [{ stacked: true, gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }], xAxes: [{ gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }] } }} />
+                            </div>
+                        </div>
+                    
+                        
+                    </Col>
+                    </Card>
+                    
+                    
+                </Row>
+            </CardBody>
+        </Card>
+        <Card>
+            <CardBody>
+                {/* <div className="d-flex align-items-center">
+                    <div>
+                        <CardTitle>Overview</CardTitle>
+                        <CardSubtitle>Details about the Graph</CardSubtitle>
+                    </div>
+                </div> */}
+                <Row>
+                    <Card>
+                    <Col lg="12">
+                        <div className="campaign ct-charts">
+                            <div className="chart-wrapper" style={{ width: '100%', margin: '5 auto', height: 250 }}>
+                                <Line data={lineData} options={{ maintainAspectRatio: false, legend: { display: false, labels: { fontFamily: "Nunito Sans" } }, scales: { yAxes: [{ stacked: true, gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }], xAxes: [{ gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }] } }} />
+                            </div>
+                        </div>
+                    
+                        
+                    </Col>
+                    </Card>
+                    <Card>
+                    <Col lg="12">
+                        <div className="campaign ct-charts">
+                            <div className="chart-wrapper" style={{ width: '100%', margin: '5px  auto', height: 250 }}>
+                                <Line data={lineData} options={{ maintainAspectRatio: false, legend: { display: false, labels: { fontFamily: "Nunito Sans" } }, scales: { yAxes: [{ stacked: true, gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }], xAxes: [{ gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }] } }} />
+                            </div>
+                        </div>
+                    
+                        
+                    </Col>
+                    </Card>
+                    <Card>
+                    <Col lg="12">
+                        <div className="campaign ct-charts">
+                            <div className="chart-wrapper" style={{ width: '100%', margin: '0 auto', height: 250 }}>
+                                <Line data={lineData} options={{ maintainAspectRatio: false, legend: { display: false, labels: { fontFamily: "Nunito Sans" } }, scales: { yAxes: [{ stacked: true, gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }], xAxes: [{ gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }] } }} />
+                            </div>
+                        </div>
+                    
+                        
+                    </Col>
+                    </Card>
+                    
+                    
+                </Row>
+            </CardBody>
+            
+        </Card>
+
+        
+
+        </Card>
+
+        <Card>
+            <CardBody>
+                <div className="d-flex align-items-center">
+                    <div>
+                        <CardTitle>Biasness</CardTitle>
+                       
+                    </div>
+                </div>
+                <Row>
+                    <Card>
+                    <Col lg="12">
+                        <div className="campaign ct-charts">
+                            <div className="chart-wrapper" style={{ width: '100%', margin: '5 auto', height: 250 }}>
+                                <Line data={lineData} options={{ maintainAspectRatio: false, legend: { display: false, labels: { fontFamily: "Nunito Sans" } }, scales: { yAxes: [{ stacked: true, gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }], xAxes: [{ gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }] } }} />
+                            </div>
+                        </div>
+                    
+                        
+                    </Col>
+                    </Card>
+                    <Card>
+                    <Col lg="12">
+                        <div className="campaign ct-charts">
+                            <div className="chart-wrapper" style={{ width: '100%', margin: '5px  auto', height: 250 }}>
+                                <Line data={lineData} options={{ maintainAspectRatio: false, legend: { display: false, labels: { fontFamily: "Nunito Sans" } }, scales: { yAxes: [{ stacked: true, gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }], xAxes: [{ gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }] } }} />
+                            </div>
+                        </div>
+                    
+                        
+                    </Col>
+                    </Card>
+                    <Card>
+                    <Col lg="12">
+                        <div className="campaign ct-charts">
+                            <div className="chart-wrapper" style={{ width: '100%', margin: '0 auto', height: 250 }}>
+                                <Line data={lineData} options={{ maintainAspectRatio: false, legend: { display: false, labels: { fontFamily: "Nunito Sans" } }, scales: { yAxes: [{ stacked: true, gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }], xAxes: [{ gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }] } }} />
+                            </div>
+                        </div>
+                    
+                        
+                    </Col>
+                    </Card>
+                    
+                    
+                </Row>
+            </CardBody>
+        
+        
+            <CardBody>
+                <div className="d-flex align-items-center">
+                    {/* <div>
+                        <CardTitle>Overview</CardTitle>
+                        <CardSubtitle>Details about the Graph</CardSubtitle>
+                    </div> */}
+                </div>
+                <Row>
+                    <Card>
+                    <Col lg="12">
+                        <div className="campaign ct-charts">
+                            <div className="chart-wrapper" style={{ width: '100%', margin: '5 auto', height: 250 }}>
+                                <Line data={lineData} options={{ maintainAspectRatio: false, legend: { display: false, labels: { fontFamily: "Nunito Sans" } }, scales: { yAxes: [{ stacked: true, gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }], xAxes: [{ gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }] } }} />
+                            </div>
+                        </div>
+                    
+                        
+                    </Col>
+                    </Card>
+                    <Card>
+                    <Col lg="12">
+                        <div className="campaign ct-charts">
+                            <div className="chart-wrapper" style={{ width: '100%', margin: '5px  auto', height: 250 }}>
+                                <Line data={lineData} options={{ maintainAspectRatio: false, legend: { display: false, labels: { fontFamily: "Nunito Sans" } }, scales: { yAxes: [{ stacked: true, gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }], xAxes: [{ gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }] } }} />
+                            </div>
+                        </div>
+                    
+                        
+                    </Col>
+                    </Card>
+                    <Card>
+                    <Col lg="12">
+                        <div className="campaign ct-charts">
+                            <div className="chart-wrapper" style={{ width: '100%', margin: '0 auto', height: 250 }}>
+                                <Line data={lineData} options={{ maintainAspectRatio: false, legend: { display: false, labels: { fontFamily: "Nunito Sans" } }, scales: { yAxes: [{ stacked: true, gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }], xAxes: [{ gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }] } }} />
+                            </div>
+                        </div>
+                    
+                        
+                    </Col>
+                    </Card>
+                    
+                    
+                </Row>
+            </CardBody>
+        </Card>
+
+        
+        
         </div>
+        
+        
     );
 }
 
--- src/views/ui-components/badge.jsx
+++ src/views/ui-components/badge.jsx
@@ -1,34 +1,61 @@
 import React from 'react';
-import { Badge, Button, Card, CardBody, CardTitle, Row, Col } from 'reactstrap';
+import { Badge, Button, Card, CardBody, CardTitle, Row, Col, CardSubtitle } from 'reactstrap';
+import { Line } from 'react-chartjs-2';
+
+//Line chart
+let lineData = {
+    labels: [1, 2, 3, 4, 5, 6, 7, 8],
+    datasets: [{
+        label: 'Income',
+        borderWidth: 1,
+        backgroundColor: 'rgba(94,114,228,.1)',
+        borderColor: '#2962ff',
+        pointBorderColor: '#2962ff',
+        pointBackgroundColor: '#2962ff',
+        data: [0, 15, 6, 11, 25, 9, 18, 24]
+    }, {
+        label: 'Outcome',
+        borderWidth: 1,
+        backgroundColor: 'rgba(79,195,247,.1)',
+        borderColor: '#ffcc13',
+        pointBorderColor: '#ffcc13',
+        pointBackgroundColor: '#ffcc13',
+        data: [0, 8, 11, 22, 8, 10, 5, 21]
+    }]
+};
+
 
 const Badges = () => {
     return (
-        <div>
-            {/* --------------------------------------------------------------------------------*/}
-            {/* Row*/}
-            {/* --------------------------------------------------------------------------------*/}
-            <Row>
-                <Col xs="12" md="12" sm="12">
-                    {/* --------------------------------------------------------------------------------*/}
-                    {/* Card-1*/}
-                    {/* --------------------------------------------------------------------------------*/}
-                    <Card>
-                        <CardTitle className="bg-light border-bottom p-3 mb-0">
-                            <i className="mdi mdi-arrange-send-backward mr-2"> </i>
-                            Merit Impact Framework
-                        </CardTitle>
-                        <CardBody className="">
-                            <div>
-                                <p> Details </p>
+        <Card>
+            <CardBody>
+                <div className="d-flex align-items-center">
+                    <div>
+                        <CardTitle>Overview</CardTitle>
+                        <CardSubtitle>Details about the Graph</CardSubtitle>
+                    </div>
+                    <div className="ml-auto ">
+                        <ul className="list-inline font-12 dl mr-3 mb-0">
+                            <li className="border-0 p-0 text-info list-inline-item">
+                                <i className="fa fa-circle"></i> Security
+								</li>
+                            <li className="border-0 p-0 text-primary list-inline-item">
+                                <i className="fa fa-circle"></i> Biasness
+								</li>
+                        </ul>
+                    </div>
+                </div>
+                <Row>
+                    <Col lg="12">
+                        <div className="campaign ct-charts">
+                            <div className="chart-wrapper" style={{ width: '100%', margin: '0 auto', height: 250 }}>
+                                <Line data={lineData} options={{ maintainAspectRatio: false, legend: { display: false, labels: { fontFamily: "Nunito Sans" } }, scales: { yAxes: [{ stacked: true, gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }], xAxes: [{ gridLines: { display: false }, ticks: { fontFamily: "Nunito Sans" } }] } }} />
                             </div>
-                        </CardBody>
-                    </Card>
-                </Col>
-            </Row>
-            {/* --------------------------------------------------------------------------------*/}
-            {/* Row*/}
-            {/* --------------------------------------------------------------------------------*/}
-        </div >
+                        </div>
+                    </Col>
+                </Row>
+            </CardBody>
+        </Card>
     );
 }
 
